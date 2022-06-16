ac19facc069264d48f9e99046e7cd986b360f3c1
pj8912 <jpintofun2b@gmail.com>
2022-06-17 00:13:40 +0530

    main ui

--- index.php
+++ index.php
@@ -1,33 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include 'includes/header.php';
+main_header('HOME');
+?>
 
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Home</title>
+<div class="container">
+	<div class="card card-body col-md-4 mt-4">
+		<form action="users/login.php" method="post">
+			<div class="mb-2">
+				<input type="text" name="" class="form-control" placeholder="Email Address">
 
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
+			</div>
+			<div class="mb-2">
+				<input type="password" name="" class="form-control" placeholder="Password*">
 
-<body>
-	<div class="container">
-		<div class="card card-body col-md-4 mt-4">
-			<form action="users/login.php" method="post">
-				<div class="mb-2">
-					<input type="text" name="" class="form-control" placeholder="Email Address">
-
-				</div>
-				<div class="mb-2">
-					<input type="password" name="" class="form-control" placeholder="Password*">
-
-				</div>
-				<div class="d-grid gap-2">
-					<button class="btn btn-primary" type="submit" name="lbtn">login</button>
-				</div>
-			</form>
-		</div>
+			</div>
+			<div class="d-grid gap-2">
+				<button class="btn btn-primary" type="submit" name="lbtn">login</button>
+			</div>
+		</form>
 	</div>
+</div>
 </body>
 
 </html>

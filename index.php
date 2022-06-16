e90d932d514c65adf6a71c6f6e579d59bf72e03e
pj8912 <jpintofun2b@gmail.com>
2022-06-17 00:07:46 +0530

    main ui

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<h1>Home</h1>
+<?php
+
+require 'includes/header.php';
+require 'includes/footer.php';
+// require 'includes/header.php';
+?>
+
+<?php main_header('Home'); ?>
+<div class="container">
+	<div class="card card-body col-md-4 mt-4">
+		<form action="users/login.php" method="post">
+			<div class="mb-2">
+				<input type="text" name="" class="form-control" placeholder="Email Address">
+
+			</div>
+			<div class="mb-2">
+				<input type="password" name="" class="form-control" placeholder="Password*">
+
+			</div>
+			<div class="d-grid gap-2">
+				<button class="btn btn-primary" type="submit" name="lbtn">login</button>
+			</div>
+		</form>
+	</div>
+</div>
+<?php main_footer(); ?>
